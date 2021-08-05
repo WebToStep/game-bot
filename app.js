70a0f6e02cc08b20b84f1e430e029db09926df46
WebStep <webtostep@yandex.kz>
2021-08-06 00:26:08 +0500

    hard lesson06 end  commit

--- app.js
+++ app.js
@@ -1,47 +1,65 @@
 'use strict';
 
-const more = 'Загаданное число меньше';
-const less = 'Загаданное число больше';
-const err = 'Введи число!';
-const gameOver = 'Игра окончена';
-const youWin = 'Поздравляю, Вы угадали!!!';
+const more = 'Загаданное число меньше, осталось попыток - ',
+   less = 'Загаданное число больше, осталось попыток - ',
+   err = 'Введи число!',
+   gameOver = 'Игра окончена',
+   youWin = 'Попытки закончились, хотите сыграть еще?',
+   restart = 'Попытки закончились, хотите сыграть еще?';
 
-function alertMessage(){return text => alert(text)}
+function alertMessage() {
+   return (text, value) => {
+      if (value) {
+         alert(text + value);
+      } else {
+         alert(text);
+      }
+   };
+}
 const message = alertMessage();
 
 let game = () => {
    let num = '' + Math.floor(Math.random() * (100 - 1) + 1);
-
-   console.log('бот загадал цифру - ', num);
+   let count = 10;
 
    let qwest = () => {
       let userNum = prompt('Угадай число от 1 до 100');
-
       // Проверка ввода числа
       const numberHandler = (value) => {
-         return isNaN( parseFloat(value) && isFinite(value) );
+         return isNaN(parseFloat(value) && isFinite(value));
       };
 
-      console.log('userNum: ', userNum);
+      const countHandler = () => {
+         if (count < 1) {
+            console.log('restart: ', restart);
+            return message(restart);
+         } else {
+            qwest();
+         }
+      };
 
-      if(userNum === null){
+      if (userNum === null) {
          return message(gameOver);
-      }else if(numberHandler(userNum)){
+      } else if (numberHandler(userNum)) {
          message(err);
          qwest();
-      }else if(userNum > num){
-         message(more);
-         qwest();
-      }else if(userNum < num){
-         message(less);
-         qwest();
-      }else if(userNum === num){
-         return message(youWin);
+      } else if (userNum > num) {
+         count--;
+         console.log('count: ', count);
+         message(more, count);
+         countHandler();
+      } else if (userNum < num) {
+         count--;
+         console.log('count: ', count);
+         message(less, count);
+         countHandler();
+      } else if (userNum === num) {
+         message(youWin);
+         return game();
       }
    };
-   
+
    qwest();
-   console.dir(qwest)
-}; 
+};
 
 game();
