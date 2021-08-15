d19afc6dedf6d6356a7ca04163c1b3a3739bcc10
WebStep <webtostep@yandex.kz>
2021-08-15 22:01:38 +0500

    OOP

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Создаем AppData
-const DomElement = function (selector, height, width, bg, fontSize) {
+let DomElement = function (selector, height, width, bg, fontSize) {
    this.selector = selector;
    this.height = height;
    this.width = width;
@@ -10,32 +10,31 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 
 };
 
-DomElement.prototype.newElement = (Class, text) => {
+DomElement.prototype.newElement = function (text) {
    let newDiv = document.createElement('div');
-   
-   if (Class[0] === '#') {
-      newDiv.id = Class.slice(1);
-   } else if (Class[0] === '.') {
-      newDiv.className = Class.slice(1);
+   console.log(this);
+   if (this.selector[0] === '#') {
+      newDiv.id = this.selector.slice(1);
+   } else if (this.selector[0] === '.') {
+      newDiv.className = this.selector.slice(1);
    }
 
-   newDiv.style.cssText = `   height: ${Class.height}px;     
-                              width: ${Class.width}px;    
-                              background: #${Class.bg};    
-                              font-size: ${Class.fontSize}px; `;
+   newDiv.style.cssText = `   height: ${this.height}px;     
+                              width: ${this.width}px;    
+                              background: #${this.bg};    
+                              font-size: ${this.fontSize}px; `;
 
    newDiv.innerText = text;
-   console.log('newDiv: ', newDiv);
 
    document.body.append(newDiv);
 };
 
-const domElement = new DomElement('.wex', 100, 300, 'd3f304', 24);
-const domElement2 = new DomElement('#wex', 100, 300, 'd3d3d3', 24);
+let domElement = new DomElement('.wex', 100, 300, 'd3f304', 34);
+let domElement2 = new DomElement('#wex', 100, 300, 'd3d3d3', 24);
 
 
-domElement.newElement(domElement, 'любой текст #1');
-domElement2.newElement(domElement2, 'любой текст #2');
+domElement.newElement('Это блок с классом');
+domElement2.newElement('Это блок с id');
 
 
 
