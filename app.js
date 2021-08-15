64f5145f888ab69b08b6226b35743aff4ef119fa
WebStep <webtostep@yandex.kz>
2021-08-15 21:20:34 +0500

    get domelements work

--- app.js
+++ app.js
@@ -1,65 +1,41 @@
 'use strict';
 
-const more = 'Загаданное число меньше, осталось попыток - ',
-   less = 'Загаданное число больше, осталось попыток - ',
-   err = 'Введи число!',
-   gameOver = 'Игра окончена',
-   youWin = 'Попытки закончились, хотите сыграть еще?',
-   restart = 'Попытки закончились, хотите сыграть еще?';
+// Создаем AppData
+const DomElement = function (selector, height, width, bg, fontSize) {
+   this.selector = selector;
+   this.height = height;
+   this.width = width;
+   this.bg = bg;
+   this.fontSize = fontSize;
 
-function alertMessage() {
-   return (text, value) => {
-      if (value) {
-         alert(text + value);
-      } else {
-         alert(text);
-      }
-   };
-}
-const message = alertMessage();
-
-let game = () => {
-   let num = '' + Math.floor(Math.random() * (100 - 1) + 1);
-   let count = 10;
-
-   let qwest = () => {
-      let userNum = prompt('Угадай число от 1 до 100');
-      // Проверка ввода числа
-      const numberHandler = (value) => {
-         return isNaN(parseFloat(value) && isFinite(value));
-      };
-
-      const countHandler = () => {
-         if (count < 1) {
-            console.log('restart: ', restart);
-            return message(restart);
-         } else {
-            qwest();
-         }
-      };
-
-      if (userNum === null) {
-         return message(gameOver);
-      } else if (numberHandler(userNum)) {
-         message(err);
-         qwest();
-      } else if (userNum > num) {
-         count--;
-         console.log('count: ', count);
-         message(more, count);
-         countHandler();
-      } else if (userNum < num) {
-         count--;
-         console.log('count: ', count);
-         message(less, count);
-         countHandler();
-      } else if (userNum === num) {
-         message(youWin);
-         return game();
-      }
-   };
-
-   qwest();
 };
 
-game();
+DomElement.prototype.newElement = (Class, text) => {
+   let newDiv = document.createElement('div');
+   
+   if (Class[0] === '#') {
+      newDiv.id = Class.slice(1);
+   } else if (Class[0] === '.') {
+      newDiv.className = Class.slice(1);
+   }
+
+   newDiv.style.cssText = `   height: ${Class.height}px;     
+                              width: ${Class.width}px;    
+                              background: #${Class.bg};    
+                              font-size: ${Class.fontSize}px; `;
+
+   newDiv.innerText = text;
+   console.log('newDiv: ', newDiv);
+
+   document.body.append(newDiv);
+};
+
+const domElement = new DomElement('.wex', 100, 300, 'd3f304', 24);
+const domElement2 = new DomElement('#wex', 100, 300, 'd3d3d3', 24);
+
+
+domElement.newElement(domElement, 'любой текст #1');
+domElement2.newElement(domElement2, 'любой текст #2');
+
+
+
